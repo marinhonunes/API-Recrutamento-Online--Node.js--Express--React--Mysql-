e5d0345b0eb6a3d706a53a0e5c5e577ca0a023f0
Mario Nunes <marionunes_azevedo@hotmail.com>
2024-04-09 19:43:17 -0300

    DataBase Connection

---
+++ BackEnd/models/database.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql2/promise');
+
+class Database {
+    constructor() {
+        this.pool = mysql.createPool({
+            host: process.env.HOST_DB,
+            user: process.env.USER_DB,
+            password: process.env.PASSWORD_DB,
+            database: process.env.DATA_BASE
+        })
+    }
+
+    async executaComando(sql, params = []) {
+        const connection = await this.pool.getConnection()
+        try {
+            const [rows] = await connection.execute(sql, params)
+            return rows
+        } finally {
+            connection.release()
+        }
+    }
+
+
+    async executaComandoNonQuery(sql, params = []) {
+        const connection = await this.pool.getConnection()
+        try {
+            const [results] = await connection.execute(sql, params)
+            return results.affectedRows
+        } finally {
+            connection.release()
+        }
+    }
+}
+
+module.exports = Database
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
 const porta = 3001;
+
 
 app.use(express.json());
 
 app.use(cors({origin: "http://localhost:3000", credentials: true}));
 
-const candidatoRoutes = require('./routes/candidatoRoutes');
-app.use('/candidato', candidatoRoutes);
+dotenv.config();
 
-const vagaRoutes = require('./routes/vagaRoutes');
-app.use('/vaga', vagaRoutes);
+// const candidatoRoutes = require('./routes/candidatoRoutes');
+// app.use('/candidato', candidatoRoutes);
 
-const entrevistaRoutes = require('./routes/entrevistaRoutes');
-app.use('/entrevista', entrevistaRoutes);
+// const vagaRoutes = require('./routes/vagaRoutes');
+// app.use('/vaga', vagaRoutes);
+
+// const entrevistaRoutes = require('./routes/entrevistaRoutes');
+// app.use('/entrevista', entrevistaRoutes);
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}`);
